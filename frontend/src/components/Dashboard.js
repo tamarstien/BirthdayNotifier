d9121f688204fb706402d7f7237317a3ebaa63a0
tamarstien <ts0533110485@gmail.com>
2024-08-06 18:59:23 +0300

    func filter by month

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 import React, {useEffect,useState} from 'react';
 
+
 const Dashboard = ()=>{
 
     const [contacts, setContacts] = useState([]);
-
+    const [filteredContacts, setFilteredContacts] = useState([]);
 
     useEffect(()=>{
         // console.log("dina")
         const fetchContacts=async()=>{
-            console.log("dinahhhhhhhhhhhhhhhhhhhhhhhhh")
         try{
             const response= await axios.get('http://localhost:4000/user/')
             setContacts(response.data);
-            console.log(response.data[0]._id);
+            // console.log(response.data[0]._id);
            
         }catch{
             console.error("error");
@@ -21,15 +21,32 @@ const Dashboard = ()=>{
         fetchContacts();
     },[]);
 
+    useEffect(() => {
+        const filterContactsByCurrentMonth = () => {
+          const currentMonth = new Date().getMonth()+1;
+        //   console.log(currentMonth)
+        //   console.log('currentMonth')
+          const contactsInCurrentMonth = contacts.filter(contact => {
+            const contactMonth = new Date(contact.birthDate).getMonth()+1;
+            console.log(contactMonth)
+            return contactMonth === currentMonth;
+          });
+          setFilteredContacts(contactsInCurrentMonth);
+        };
+    
+        filterContactsByCurrentMonth();
+      }, [contacts]);
+
     return (
         <div>/
-            {/* // קריאת שרת גט לקבלת כל המשתמשים
-            //פלטור לפי החודש הקרוב
+            {/* // קריאת שרת גט לקבלת כל המשתמשים ☝
+            //פלטור לפי החודש הקרוב-
+            לראות את התאריך של החודש ולהציג את כל מי שהחודש שלו שווה לתאריך
             //הצגת המשתמשים בטבלה
             <h1>Dashboard</h1> */}
             <p>Welcome to your dashboard!</p>
             <ul>
-        {contacts.map(contact => (
+        {filteredContacts.map(contact => (
           <li key={contact.id}>
             {contact.userName} - {new Date(contact.birthDate).toLocaleDateString()}
           </li>
